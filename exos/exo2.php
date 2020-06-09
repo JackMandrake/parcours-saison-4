56a244deb0a0da389d329b2ddc9b8cdfb5f2d231
JackMandrake <coignard.cyrille@hotmail.fr>
2020-06-09 09:19:03 +0200

    Please respect my privacy-exo2 finished

--- exos/exo2.php
+++ exos/exo2.php
@@ -19,8 +19,27 @@ require_once '../inc/functions.php';
  *      echo $mario->lives; // Fatal Error
  */
 
+ // Création de la classe Mario (on aurais pu changer pour Wario ou Luigi)
+class Mario {
 
+    // On déclare la propriété de la classe mais cette fois ci en "private"
+    // inaccessible en dehors de la classe
+    private $lives;
 
+    // On déclare la méthode "construct" afin de lui mettre le paramètre "vies" comme sur l'exo1
+    public function __construct($lives = 3) {
+        // Toujours pareil à l'exo1
+        $this->lives = $lives;
+    }
+
+    // Pour récupérer le nombre de vie, on créer une fonction pour renvoyer le Nb de vie en dehors de la classe
+    public function getLives() {
+        // et on retourne la variable "vies"
+        return $this->lives;
+    }
+}
+
+// Je suis curieux de savoir si tous les commentaires sont lu...
 
 
 
